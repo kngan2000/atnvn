5a311d24b2127ed02cb1a28d9a3fea3663d8af37
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2021-11-11 22:54:19 +0700

    ư

--- Search_Product.php
+++ Search_Product.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>ATN COMPANY</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/price-range.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>ATN COMPANY</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/prettyPhoto.css" rel="stylesheet">
+	<link href="css/price-range.css" rel="stylesheet">
+	<link href="css/animate.css" rel="stylesheet">
 	<link href="css/main.css" rel="stylesheet">
 	<link href="css/banner.css" rel="stylesheet">
-	     
-</head><!--/head-->
+
+</head>
+<!--/head-->
+<?php
+Session_Start();
+include_once("connection.php");
+?>
 
 <!-- The dots/circles -->
 <div style="text-align:center">
-<span class="dot" onclick="currentSlide(1)"></span> 
-<span class="dot" onclick="currentSlide(2)"></span> 
-<span class="dot" onclick="currentSlide(3)"></span> 
+	<span class="dot" onclick="currentSlide(1)"></span>
+	<span class="dot" onclick="currentSlide(2)"></span>
+	<span class="dot" onclick="currentSlide(3)"></span>
 </div>
 
 <body>
-	<header id="header"><!--header-->
-		<div class="header_top"><!--header_top-->
+	<header id="header">
+		<!--header-->
+		<div class="header_top">
+			<!--header_top-->
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-6">
@@ -49,7 +57,111 @@
 					</div>
 				</div>
 			</div>
-</body>
+		</div>
+		<!--/header_top-->
+		<div class="header-middle">
+			<!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="">
+						<div class="logo">
+							<a href="?page=content"><img src="image/logoatn.jpg" style="width: 20%; display: block; margin-left: auto; margin-right: auto;" alt=""></a>
+						</div>
+					</div>
+					<div class="col-sm-8 pull-right">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<?php
+								if (isset($_SESSION['username']) && $_SESSION['username'] != "") {
+								?>
+									<li><a class="tm-nav-link" href="?page=update_customer"><i class="fa fa-user"></i>Hi,&nbsp;<?php echo $_SESSION['username']; ?> </a>
+									</li>
+									</li>
+									<li><a class="tm-nav-link" href="?page=logout">
+											<i class="fa fa-crosshairs"></i>Logout</a></li>
+								<?php
+								} else {
+								?>
+									<li class="tm-nav-li"><a href="?page=login" class="tm-nav-link"><i class="fa fa-user"></i>Login</a></li>
+									<li><a href="?page=Register"><i class="fa fa-lock"></i> Register</a></li>
+
+								<?php
+								}
+								?>
+							</ul>
+						</div>
+					</div>
+				</div>
+</div>
+		</div>
+		<!--/header-middle-->
+		<div class="header-bottom">
+			<!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.php">Home</a></li>
+								
+								<?php
+								if (isset($_SESSION['username']) && $_SESSION['admin'] == 1) {
+								?>
+								<li class="dropdown"><a href="?page=shop">Shop</a>
+								</li>
+									<li class="dropdown"><a href="#">Product<i class="fa fa-angle-down"></i></a>
+										<ul role="menu" class="sub-menu">
+											<li><a href="?page=add_product">Add Product</a></li>
+											<li><a href="?page=product_management"> Product management</a></li>
+										</ul>
+									</li>
+									<li class="dropdown"><a href="#">Category<i class="fa fa-angle-down"></i></a>
+										<ul role="menu" class="sub-menu">
+											<li><a href="?page=Add_Category">Add Category</a></li>
+											<li><a href="?page=category_management">Category Management</a></li>
+
+										</ul>
+									</li>
+
+									<li class="dropdown"><a href="#">Store<i class="fa fa-angle-down"></i></a>
+										<ul role="menu" class="sub-menu">
+											<li><a href="?page=add_store">Add Store</a></li>
+											<li><a href="?page=store">Store Management</a></li>
+
+										</ul>
+									</li>
+
+								<?php
+								} else {
+								?>
+										<li class="nav-item"><a href="?page=shop">Shop</a></li>
+								<?php
+								}
+								?>
+
+						</div>
+						<div class="container">
+							<form action="Search_Product.php" method="POST">
+								<div class="col-sm-3  pull-right">
+									<div class="search_box pull-right">
+										<input type="text" name="txtSearch" placeholder="Search" />
+										<button class="btn btn-success search" type="submit" name="search" style=" background-color: #; border-color: #ffffff;">Search</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+    </header>
+<!--/header-bottom-->
 <?php
 if (isset($_POST['txtSearch'])) {
 
